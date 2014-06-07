d74257926020b5618f3e64a252c3444701a54d32
Danny Qiu <dqiu55@gmail.com>
2014-06-07 19:58:11 -0400

    Change styling of and affix navigation bar

--- js/main.js
+++ js/main.js
@@ -3,9 +3,22 @@ $(window).resize(calculateHeight);
 function calculateHeight() {
     w = $(window).width();
     h = $(window).height();
-    $('#main > div').height(h);
-    $('#main img').height(h/2);
+    offset = $('#navigation').height();
+    $('#main > div').height(h - offset);
+    $('#main img').height((h - offset)/2);
 }
+$('#navigation').affix({
+    offset: {
+        top: function() {return $('#main').height();}
+    }
+});
+$('#navigation').on('affix.bs.affix', function() {
+    $('.tab-content').css('padding-top', $(this).height());
+});
+$('#navigation').on('affix-top.bs.affix ', function() {
+    $('.tab-content').css('padding-top', 0);
+});
+
 /*
 $(function() {
     $('a[href*=#]:not([href=#])').click(function() {
